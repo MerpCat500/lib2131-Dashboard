905918493f28eabf8e6536df7b248c9b85efd3dd
Andrew Hilton <merpleton@gmail.com>
2024-03-28 23:29:48 -0600

    PID Comments

--- lib2131/include/lib2131/movement/pid.hpp
+++ lib2131/include/lib2131/movement/pid.hpp
@@ -6,9 +6,21 @@
 
 namespace lib2131
 {
-
+/**
+ * PID CONTROLS
+ * @brief Control Values for a PID
+ */
 struct PID_Controls
 {
+  /**
+   * PID_Controls
+   * @param  double kP : P tuning Constant
+   * @param  double kI : I tuning Constant
+   * @param  double kD : D tuning Constant
+   * @param  PID_Exit Exit_Conditions : Exit Conditions For PID
+   * @param  bool isVelocity : If PID to control a Velocity Target (Flywheels)
+   */
+
   double kP;
   double kI;
   double kD;
@@ -18,8 +30,20 @@ struct PID_Controls
   bool isVelocity;
 };
 
+/**
+ * PID EXIT CONDITIONS
+ * @brief Exit Conditions for a PID
+ */
 struct PID_Exit
 {
+  /**
+   * PID_Exit
+   * @param double _small_error : Error that is constituted as small
+   * @param double _small_time : Time within a small error before being settled
+   * @param double _large_error : Error that is constituted as large
+   * @param double _large_time : Time within a large error before being settled
+   */
+
   double _small_error;
   double _small_time;
 
@@ -27,26 +51,58 @@ struct PID_Exit
   double _large_time;
 };
 
+/**
+ * PID CLASS
+ * @brief Class for a PID
+ */
 class PID
 {
  private:
-  PID_Controls _Controls;
-  timer _timer;
-  double _last_error;
-  double _last_I;
+  PID_Controls _Controls;  // Controls
+  timer _timer;            // Timer for Settled
+  double _last_error;      // Error from last update
+  double _last_I;          // Last Integral Error for TBH
 
-  bool _settled;
+  bool _settled;  // Is PID Settled?
 
-  double P, I, D;
+  double P, I, D;  // P, I, D Outputs
 
  public:
+  /**
+   * PID
+   * @brief Constructor for a PID Controller
+   * @param PID_Controls Controls : Controls for the PID
+   */
   PID(PID_Controls Controls);
+  /**
+   * PID
+   * @brief Constructor for a PID Controller
+   * @param PID_Controls Controls : Controls for the PID
+   * @param timer Timer : Timer for PID
+   */
   PID(PID_Controls Controls, timer Timer);
-
+  /**
+   * UPDATE PID
+   * @brief Get a PID output
+   * @param  double error : Error = Target - Actual
+   * @return double      : PID * kPID
+   */
   double update(double error);
+  /**
+   * IS PID SETTLED
+   * @brief Returns if PID is settled
+   * @return bool : Settled
+   */
   bool get_settled();
+  /**
+   * Returns if PID is a velocity PID
+   * @return bool  : is Velocity PID?
+   */
   bool get_Type();
-
+  /**
+   * RESET PID
+   * @brief Resets PID
+   */
   void reset();
 };
 
--- lib2131/src/lib2131/movement/pid.cpp
+++ lib2131/src/lib2131/movement/pid.cpp
@@ -2,63 +2,104 @@
 
 namespace lib2131
 {
+/**
+ * PID Constructor
+ * @brief Constructs a PID
+ * @param PID_Controls Controls : Controls for a PID
+ */
 PID::PID(PID_Controls Controls)
     : _timer(timer()), _Controls(Controls), _last_I(0), _last_error(0)
 {
 }
+/**
+ * PID Constructor
+ * @brief Constructs a PID
+ * @param PID_Controls Controls : Controls for a PID
+ * @param timer Timer : A timer to be used
+ */
 PID::PID(PID_Controls Controls, timer Timer)
     : _timer(Timer), _Controls(Controls), _last_I(0), _last_error(0)
 {
 }
-
+/**
+ * Update PID
+ * @brief update a PID, calculate new output
+ * @param  double error : Error = Target - Actual
+ * @return double       :  PID * kPID
+ */
 double PID::update(double error)
 {
-  double Output(0);
-  if (_Controls.isVelocity)
+  double Output(0);          // Init Output
+  if (_Controls.isVelocity)  // Is Velocity Controller
   {
-    P = error;
+    P = error;  // P = Error
+    // Trapezoidal Riemann Sum
     I += (error + _last_error) * _timer.get_time() / 2;
+    // If Error's Sign Changed
     if (I * _last_I < 0)
     {
-      I = 0;
+      I = 0;  // Remove I Component
     }
+    // dError / dTime
     D = (error - _last_error) / _timer.get_time();
 
+    // Update Last
     _last_error = error;
     _last_I = I;
+
+    // Output PID * kPID
     Output = P * _Controls.kP + I * _Controls.kI + D * _Controls.kD;
   }
   else
   {
-    P = error;
+    P = error;  // P = Error
+    // Trapezoidal Riemann Sum
     I += (error + _last_error) * _timer.get_time() / 2;
+    // dError / dTime
     D = (error - _last_error) / _timer.get_time();
 
+    // Update Last
     _last_error = error;
     _last_I = I;
+
+    // Ouput PID * kPID
     Output = P * _Controls.kP + I * _Controls.kI + D * _Controls.kD;
   }
 
+  // Calculate Settled
+  // if In large Error zone and Over Large Settled Time
   if (fabs(Output) < _Controls.Exit_Conditions._large_error &&
       _timer.get_time() > _Controls.Exit_Conditions._large_time)
   {
-    _settled = true;
+    _settled = true;  // Is Settled
   }
+  // If in Small Error zone and Over Small Settled Time
   else if (fabs(Output) < _Controls.Exit_Conditions._small_error &&
            _timer.get_time() > _Controls.Exit_Conditions._small_time)
   {
-    _settled = true;
+    _settled = true;  // Is Settled
   }
-  else
+  else  // Otherwise
   {
-    _settled = false;
+    _settled = false;  // Isn't Settled
   }
 }
-
+/**
+ * Get Settled
+ * @brief Get if PID is settled
+ * @return bool : is Settled?
+ */
 bool PID::get_settled() { return _settled; }
-
+/**
+ * Get Type
+ * @brief Get if PID is a Velocity PID
+ * @return bool : is Velocity?
+ */
 bool PID::get_Type() { return _Controls.isVelocity; }
-
+/**
+ * Reset PID
+ * @brief Reset PID Values and Timer
+ */
 void PID::reset()
 {
   P = 0;
